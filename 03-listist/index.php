fa5467cbd086d3c4d2cc84b672396cef9a2be42a
samua407 <samua407@newschool.edu>
2013-12-04 14:11:22 -0700

    updated css

--- 03-listist/index.php
+++ 03-listist/index.php
@@ -2,32 +2,42 @@
 <head>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="lib/app.js"></script>
-<script src="lib/main.js"></script>
+<script src="lib/js/app.js"></script>
+<script src="lib/js/main.js"></script>
 
 
-<link rel="stylesheet" href="lib/normalize.min.css">
-<link rel="stylesheet" href="lib/main.css">
+<link rel="stylesheet" href="lib/style/normalize.min.css">
+<link rel="stylesheet" href="lib/style/main.css">
 
 </head>
 
 <body>
 	
 <div id='header'>
-	<div class="select" id="gf">
-	Gluten Free
+	<div class="type">
+		<h2>TYPE:</h2> <p></p>
 	</div>
-	<div class="select" id="fish">
-	Fish
-	</div>
-	<div class="select" id="meat">
-	Meat
-	</div>
-	<div class="select" id="vegan">
-	Vegan
-	</div>
-	<div class="select" id="veg">
-	Vegetarian
+		
+	<div id='type'>
+
+		<div class="select" id="gf">
+		Gluten Free
+		</div>
+		<div class="select" id="fish">
+		Fish
+		</div>
+		<div class="select" id="meat">
+		Meat
+		</div>
+		<div class="select" id="vegan">
+		Vegan
+		</div>
+		<div class="select" id="veg">
+		Vegetarian
+		</div>
+		<div class="select" id="all">
+		All
+		</div>
 	</div>
 </div>
 
---
+++ 03-listist/lib/js/main.js
@@ -0,0 +1,321 @@
+var email = '';
+
+var chooser = {
+	gfCheck: '',
+	fishCheck: '',
+	meatCheck: '',
+	veganCheck: '',
+	vegCheck: '',
+
+	listen: function(){
+		
+		$('div.type').click(function(e){
+			$('div#type').slideToggle();
+		});
+		
+		$('div.select').click(function(e){
+			$('div.select').unbind('click');
+						
+			var div = $(e.target);
+			div.removeClass('select').addClass('select-active');
+			
+			$('ul.recipe').css('display', 'none');
+		    
+		    switch(e.target){
+			   case gf:
+					chooser.gfCheck = 'active';
+					chooser.activate();
+			   break;
+			    
+			   case fish:
+			  		chooser.fishCheck = 'active';
+					chooser.activate();
+			   break;
+			    
+			   case meat:
+					chooser.meatCheck = 'active';
+					chooser.activate();
+			   break;
+			   
+			   case vegan:
+					chooser.veganCheck = 'active';
+					chooser.activate();
+			   break;
+			   
+			   case veg:
+					chooser.vegCheck = 'active';
+					chooser.activate();
+			   break;
+			   
+			   case all:
+		   			$('div.select').addClass('select-active');
+			   		chooser.gfCheck = 'active';
+			   		chooser.fishCheck = 'active';
+			   		chooser.meatCheck = 'active';
+			   		chooser.veganCheck = 'active';
+			   		chooser.vegCheck = 'active';		   		
+					chooser.activate();
+			   		
+			   	break;
+			   
+		    }
+		    
+		});
+
+		$('div.select-active').click(function(e){
+		
+			var div = $(e.target);
+			div.removeClass('select-active').addClass('select');
+			
+			$('ul.recipe').css('display', 'none');
+
+			switch(e.target){
+			    case gf:
+					chooser.gfCheck = 'inactive';
+					chooser.activate();
+			    break;
+			    
+			    case fish:
+					chooser.fishCheck = 'inactive';
+					chooser.activate();
+			    break;
+			    
+			   case meat:
+					chooser.meatCheck = 'inactive';
+					chooser.activate();
+			   break;
+			   
+			   case vegan:
+					chooser.veganCheck = 'inactive';
+					chooser.activate();
+			   break;
+			   
+			   case veg:
+					chooser.vegCheck = 'inactive';
+					chooser.activate();
+			   break;
+			   
+			   case all:
+   		   			$('div.select-active').removeClass('select-active').addClass('select');
+			   		chooser.gfCheck = 'inactive';
+			   		chooser.fishCheck = 'inactive';
+			   		chooser.meatCheck = 'inactive';
+			   		chooser.veganCheck = 'inactive';
+			   		chooser.vegCheck = 'inactive';	
+			   		chooser.activate();
+			   	break;
+		    }
+
+	
+		});	
+		
+	},
+	activate: function(){
+		
+		//console.log('activating!');
+		
+		$('div.type p ').empty();
+
+
+		if(chooser.gfCheck === 'active'){
+			//console.log('gf is active');
+			$('#recipes').find('ul.gf').css('display', 'block');
+			$('div.type p ').append(' GF');
+		}
+		if(chooser.fishCheck === 'active'){
+			//console.log('fish is active');
+			$('#recipes').find('ul.fish').css('display', 'block');
+			$('div.type p ').append(' Fish');
+		}
+		if(chooser.meatCheck === 'active'){
+			//console.log('meat is active');
+			$('#recipes').find('ul.meat').css('display', 'block');
+			$('div.type p ').append(' Meat');
+		}
+		if(chooser.veganCheck === 'active'){
+			//console.log('vegan is active');
+			$('#recipes').find('ul.vegan').css('display', 'block');
+			$('div.type p ').append(' Vegan');
+		}
+		if(chooser.vegCheck === 'active'){
+			//console.log('veg is active');
+			$('#recipes').find('ul.veg').css('display', 'block');
+			$('div.type p ').append(' Veg.');
+		}
+		
+		if(chooser.all === 'active'){
+			$('#reciples').css('display', 'block');
+		}
+		
+	    chooser.listen();
+	    view.listen();
+	}
+	
+
+};
+
+
+var view = {
+	
+	listen: function(){
+
+		$('ul.recipe h1').click(function(e){
+			$('ul.recipe h1').unbind('click');
+			$('ul.recipefull').removeClass('recipefull').addClass('recipe');
+			$(e.target.parentElement).removeClass('recipe').addClass('recipefull');
+			view.listen();
+			view.listen_shrink();
+			shop.listen();
+		});
+		
+	},
+	
+	listen_shrink: function(){
+		
+		$('ul.recipefull h1').click(function(e){
+		
+			$('ul.recipefull').removeClass('recipefull').addClass('recipe');
+			
+		});
+	},
+	
+	toggle_open: function(){
+		$('div#select-toggle').click(function(e){
+		
+			$('div#select-toggle').css('display', 'none');
+			
+		});
+	}
+	
+	
+};
+
+
+var shop = {
+
+	list: new Array(),
+	
+	listen: function(){
+				
+		$('ul.recipefull li').click(function(e){
+			$('ul.recipefull li').unbind('click');
+			$(e.target).addClass('shop');
+			
+			var submit = e.target.parentElement;//.childNodes;
+			submit = $(submit).find('.submit')[0];
+			$(submit).fadeIn();
+
+			
+			var val = $(e.target)[0].textContent;
+			shop.list.push(val);
+			
+			shop.listen_cancel();
+						
+			shop.button(e);
+			
+		});
+	},
+
+	listen_cancel: function(){
+		
+		shop.listen();
+
+		$('li.shop').click(function(e){
+		
+			$(e.target).removeClass('shop');
+			
+			var val = $(e.target)[0].textContent;   
+			 
+			shop.list = $.grep(shop.list, function(value) {
+			  return value != val;
+
+			});
+			
+			shop.button(e);
+
+		});
+		
+	},
+	
+	button: function(e){
+	
+	
+		$( 'div.submit' ).click(function(e){
+			e.preventDefault();
+			shop.send(e);
+		});
+		
+		if(shop.list.length === 0 ){
+			var submit = e.target.parentElement;//.childNodes;
+			submit = $(submit).find('.submit')[0];
+			$(submit).fadeOut();
+		}
+		
+	},
+	
+	check: function(){
+		var atpos=email.indexOf("@");
+		var dotpos=email.lastIndexOf(".");
+		if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length){
+			return false;
+		}
+	},
+			
+	send: function(e){
+			
+			$(e).unbind('click');
+			
+			if(email === ''){
+				email = prompt("Where do you want to send your list? ", "enter an email address here");
+				if(shop.check() === false){
+					alert('This isn\'t a valid e-mail address. Click "send" and try again.');  
+				}
+			}else{
+				
+				var check = shop.check();
+				if(check === false){
+					email = prompt("Where do you want to send your list? ", "enter an email address here");
+				}
+			}
+			
+			var info = e.target.parentElement;
+			var name = $(info).find('h1')[0].innerText; //.find('name');
+			var ingreds = shop.list;
+			var url = $(info).find('h3');
+			url = $(url)[0];
+			url = $(url).find('a')[0].href;
+			
+
+			var num = ingreds.length;
+			var ingredString = '';
+
+			for(i = 0; i<num; i++){
+			
+				ingredString = ingredString + ingreds[i]+ '<br>' ;
+			
+			}
+			
+			
+			var link = 'lib/mail.php?email='+email+'&ingreds='+ingredString+'&name='+name+'&url='+url;
+		
+		   	console.log(link);
+		   	window.open(link,"loader");
+		   	alert('sent!');
+		   	
+		   	
+			
+		  	 
+		
+	},
+	
+
+};
+
+
+  
+$( document ).ready(function() {
+
+	chooser.listen();	
+  
+  
+});
